f89d4b5bca5e2446de08c517b46e83b956d54016
agrigaliunas <agustingrigaliunas@gmail.com>
2022-11-25 11:23:01 -0300

    added bootstrap styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 const App = () => {
   return (
     <>
       <Header />
-      <main>
-        <h1>Bienvenido a RapiTienda</h1>
+      <main className="py-3">
+        <Container>
+          <h1>Bienvenido a RapiTienda</h1>
+        </Container>
       </main>
       <Footer />
     </>
