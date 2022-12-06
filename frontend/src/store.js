ed9dc83b0b6f68aed895003a71322dafe56e16c8
agrigaliunas <agustingrigaliunas@gmail.com>
2022-12-05 22:48:39 -0300

    add to cart

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,13 +2,23 @@ import { createStore, combineReducers, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
+import { cartReducer } from './reducers/cartReducers';
+
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    cart: cartReducer
 });
 
-const initalState = {};
+
+const cartItemsFromStorage = JSON.parse(localStorage.getItem('cartItems')) || [];
+
+const initalState = {
+    cart: {
+        cartItems: cartItemsFromStorage
+    }
+};
 
 const middleware = [thunk];
 const store = createStore(reducer, initalState, composeWithDevTools(applyMiddleware(...middleware)));
