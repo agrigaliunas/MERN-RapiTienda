6c18997071edf3d8b4fbc13a6acdc516e8b402fb
agrigaliunas <agustingrigaliunas@gmail.com>
2022-12-06 02:06:36 -0300

    update profile controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,13 +3,14 @@ import {
   authUser,
   getUserProfile,
   registerUser,
+  updateUserProfile
 } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+router.route("/").post(registerUser);
 router.post("/login", authUser);
-router.post("/", registerUser);
-router.route("/profile").get(protect, getUserProfile);
+router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile);
 
 export default router;
